d54740e1572a943b8d0d3003f285727b772f44d3
Leonardo Furtado <leonardo.k@escolar.ifrn.edu.br>
2024-08-27 19:28:51 +0000

    continuação do exercicio 2 + chatGPT teste

--- exercicio2/solucao2js.js
+++ exercicio2/solucao2js.js
@@ -1,16 +1,26 @@
-async function gerarbarraPesquisa() {
+const sugestoes = document.querySelector('#sugestoes');
+const barra_pesquisa = document.querySelector('#barra_pesquisa');
+
+async function processarDados() {
     let data = await fetch('https://worldcupjson.net/matches');
     data = await data.json();
+    return data;
+} 
+
+async function gerarbarraPesquisa() {
     let matchlist = [];
     let matchlist_html = [];
     let li_counter = 0;
-    const sugestoes = document.querySelector('#sugestoes')
+    let data = await processarDados();
+
+    sugestoes.style.display = 'block';
+
     data.forEach(match => {
-        const text = `${match['home_team']['name']} x ${match['away_team']['name']} ${match['datetime'].slice(0,9)} ${match['datetime'].slice(11,18)}`;
-        matchlist.push(text);
+        let template = `${match['home_team']['name']} x ${match['away_team']['name']} ${match['datetime'].slice(0,9)} ${match['datetime'].slice(11,18)}`;
+        matchlist.push(template);
 
         const item = document.createElement('div');
-        item.innerHTML = text;
+        item.innerHTML = template;
         item.addEventListener('click', Event => {
             document.querySelector('#barra_pesquisa').value = item.innerHTML;
             sugestoes.style.display = 'none';
@@ -20,14 +30,52 @@ async function gerarbarraPesquisa() {
         sugestoes.appendChild(item);
     });
 
-    sugestoes.addEventListener('input', Event => {
+    barra_pesquisa.addEventListener('input', Event => {
         for (let values of matchlist) {
-            if (!values.includes(this.value)) {
-                matchlist_html[values.find(this.value)].style.display = 'none';
-                console.log('oi');
-            };
+            if (values.includes(Event.currentTarget.value)) {
+                matchlist_html[matchlist.indexOf(values)].style.display = 'block';
+            } else {
+                matchlist_html[matchlist.indexOf(values)].style.display = 'none'
+            }
         };
     });
+};
+
+async function gerarDetalhes() {
+    sugestoes.style.display = 'block';
+    let valor_entrada = barra_pesquisa.value;
+    let data = await processarDados();
+    if (!barra_pesquisa.value) {
+        sugestoes.style.display = 'none';
+        alert('Preencha o campo corretamente!');
+        return gerarbarraPesquisa();
+    }
+    data.forEach(match => {
+        let template = `${match['home_team']['name']} x ${match['away_team']['name']} ${match['datetime'].slice(0,9)} ${match['datetime'].slice(11,18)}`;
+        if (template === valor_entrada) {
+            return console.log(await displayDetalhes(match));
+        }
+    })
+};
+
+async function displayDetalhes(object) {
+    let match_info = await fetch(`https://worldcupjson.net/matches/${object.id}/`);
+    match_info = await match_info.json();
+    return percorrerDetalhes(match_info, 'Datalhes do jogo: <br>');
+
+};
+
+function percorrerDetalhes(objeto, text) {
+    let keys = Object.keys(objeto);
+    keys.forEach(chave => {
+        if (typeof objeto[chave] === 'object' && objeto[chave] !== null ) {
+            text = text + `${chave}: <br>     ${percorrerDetalhes(objeto[chave], text)}`;
+        }
+        else {
+            text = `${text}<br>${chave}: ${objeto[chave]}`;
+        }
+    })
+    return text;
 }
 gerarbarraPesquisa();
 
