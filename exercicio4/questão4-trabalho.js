e8ba98d96cb0743ab97ef0febe4e99e80e14ea79
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2024-09-06 07:42:00 -0300

    mudança na cor de fundo e código comentado

--- exercicio4/questão4-trabalho.js
+++ exercicio4/questão4-trabalho.js
@@ -1,24 +1,24 @@
 url = "https://worldcupjson.net/teams"
 
 fetch(url)
-    .then(response => response.json())
+    .then(response => response.json()) //básico, só pra "pegar" o json
     .then(data => displaygroups(data))
 
     function displaygroups(json){
         const tabelas = document.getElementById("tabelas")
-        json.groups.forEach(group => {
-            let tabela = document.createElement("table")
-            let head = document.createElement("thead")
-            let title = document.createElement("h3")
+        json.groups.forEach(group => { //percorre os grupos
+            let tabela = document.createElement("table") //cria o elemento da tabela
+            let head = document.createElement("thead") //cria o cabeçalho
+            let title = document.createElement("h3") //cria o título informando o grupo
 
-            tabelas.appendChild(title)
-            title.innerHTML = `<h3>Grupo ${group.letter}</h3>`
+            tabelas.appendChild(title) //adiciona o título à div tabelas
+            title.innerHTML = `<h3>Grupo ${group.letter}</h3>` //escreve o título
             head.innerHTML = `
                 <th>Nome</th><th>Pontos</th><th>Vitórias</th><th>Empates</th><th>Derrotas</th><th>Jogos</th><th>Gols a favor</th><th>Gols contra</th><th>Diferença</th>
-            `
+            ` //define o cabeçalho
             
-            tabela.appendChild(head)
-            group.teams.forEach(team => {
+            tabela.appendChild(head) //adiciona o cabeçalho  à tabela
+            group.teams.forEach(team => { //percorre os times
                 let line = tabela.insertRow();
                 line.insertCell(0).textContent = team.name;
                 line.insertCell(1).textContent = team.group_points;
@@ -29,14 +29,15 @@ fetch(url)
                 line.insertCell(6).textContent = team.goals_for;
                 line.insertCell(7).textContent = team.goals_against;
                 line.insertCell(8).textContent = team.goal_differential;
-            });
-
+            }); //adiciona cada célula
+            
+            //isso aqui é só pra deixar ""bonito""
             tabela.style.borderWidth = "1px";
             tabela.style.borderColor = "green";
             tabela.style.borderStyle = "solid";
             tabela.style.background = "lightgreen"
             tabela.style.textAlign = "center"
 
-            tabelas.appendChild(tabela)
+            tabelas.appendChild(tabela) //adiciona cada tabela à div
         })
     }
